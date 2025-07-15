9d60a21d2d7e876b27b0ff10735f8ee5b7f74c34
Adam Mitro <adam.mitro@gmail.com>
2025-07-15 14:25:48 -0400

    Add dark theme switch to landing page. Go to section added

--- frontend/src/aboutme/aboutMe.tsx
+++ frontend/src/aboutme/aboutMe.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import './AboutMe.scss';
 import { ArrowLeftIcon } from '@heroicons/react/24/solid';
 import WorkExperienceItem from '../components/WorkExperienceItem/WorkExperienceItem';
 import { ProgrammingLanguage, IconsHelper } from '../helpers/helpers';
@@ -12,6 +11,13 @@ const AboutMe: React.FC = () => {
     jobDuties: string[];
     programmingLanguages?: ProgrammingLanguage[];
   };
+
+  enum AboutMeNavLocations {
+    WorkExperience = 'workExperience',
+    Portfolio = 'portfolio',
+    Education = 'education',
+    ContactInfo = 'contactInfo'
+  }
 
   const jobExperiences: JobExperience[] = [
     {
@@ -62,6 +68,28 @@ const AboutMe: React.FC = () => {
     }
   }, [darkMode]);
 
+  useEffect(() => {
+    const queryParam = document.URL.split('?')[1];
+    if (queryParam) {
+      const section = queryParam.split('=')[1];
+      if (section) {
+        const element = document.getElementById(section);
+        goTo(section as AboutMeNavLocations);
+      }
+    }
+    
+  }, []);
+
+  function goTo(location: AboutMeNavLocations) {
+    const element = document.getElementById(location);
+    if (element) {
+      window.scrollTo({
+        top: element.offsetTop,
+        behavior: 'smooth'
+      });
+    }
+  }
+
   return (
     <div id="aboutMeContainer" className="font-mono bg-primary">
       <div className="w-full h-20 bg-cardBg border-b-2 border-b-secondary flex shadow-lg">
@@ -71,17 +99,17 @@ const AboutMe: React.FC = () => {
         </button>
         <div className="flex w-full justify-center">
           <div className="flex items-center w-1/2 justify-evenly">
-            <button className="h-full flex-1 flex items-center justify-center text-secondary hover:text-textMain py-auto" onClick={() => window.history.back()}>
+            <button className="h-full flex-1 flex items-center justify-center text-secondary hover:text-textMain py-auto" onClick={() => goTo(AboutMeNavLocations.WorkExperience)}>
               <span>Work Experience</span>
             </button>
-            <button className="h-full flex-1 flex items-center justify-center text-secondary hover:text-textMain py-auto" onClick={() => window.history.back()}>
+            <button className="h-full flex-1 flex items-center justify-center text-secondary hover:text-textMain py-auto" onClick={() => goTo(AboutMeNavLocations.Portfolio)}>
               <span>Portfolio</span>
             </button>
-            <button className="h-full flex-1 flex items-center justify-center text-secondary hover:text-textMain py-auto" onClick={() => window.history.back()}>
+            <button className="h-full flex-1 flex items-center justify-center text-secondary hover:text-textMain py-auto" onClick={() => goTo(AboutMeNavLocations.Education)}>
               <span>Education</span>
             </button>
-            <button className="h-full flex-1 flex items-center justify-center text-secondary hover:text-textMain py-auto" onClick={() => window.history.back()}>
-              <span>Contact Me</span>
+            <button className="h-full flex-1 flex items-center justify-center text-secondary hover:text-textMain py-auto" onClick={() => goTo(AboutMeNavLocations.ContactInfo)}>
+              <span>Contact Info</span>
             </button>
           </div>
         </div>
@@ -101,7 +129,7 @@ const AboutMe: React.FC = () => {
                 onChange={() => { }}
                 readOnly
               />
-              <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600" />
+              <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600 bg-secondary" />
               <span className="ms-3 text-sm font-mono">
                 {darkMode ? "Dark Mode" : "Light Mode"}
               </span>
@@ -110,7 +138,6 @@ const AboutMe: React.FC = () => {
         </label>
       </div>
       <div className="min-h-screen text-white py-5">
-        { }
         <section className="flex flex-col md:flex-row items-center gap-8 p-6 max-w-7xl mx-auto my-20">
           <div className="w-full md:w-1/2 ">
             <img src="Adam.png" alt="Adam Mitro" className="rounded-full shadow-lg w-full h-auto object-cover border-4 border-secondary" />
@@ -125,7 +152,7 @@ const AboutMe: React.FC = () => {
           </div>
         </section>
         {/* Work Experience Section */}
-        <section className="flex flex-col md:flex-row items-center gap-8 py-6 w-full mx-auto mb-20">
+        <section id="workExperience"  className="flex flex-col md:flex-row items-center gap-8 py-6 w-full mx-auto mb-20">
           <div className="bg-cardBg p-6 border-y border-secondary shadow-lg overflow-hidden w-full">
             <h2 className="text-secondary text-3xl font-bold mb-5 font-mono">Work Experience</h2>
             {jobExperiences.map((experience, index) => (
@@ -139,19 +166,31 @@ const AboutMe: React.FC = () => {
           </div>
         </section>
         {/* Portfolio Section */}
-        <section className="flex flex-col md:flex-row items-center gap-8 py-6 w-full mx-auto mb-20">
+        <section id="portfolio" className="flex flex-col md:flex-row items-center gap-8 py-6 w-full mx-auto mb-20">
           <div className="bg-cardBg p-6 border-y border-secondary shadow-lg w-full">
-            <h2 className="text-secondary text-3xl font-bold mb-4 font-mono">Portfolio</h2>
+            <h2 className="text-secondary text-3xl font-bold mb-4 font-mono">Portfolio Projects</h2>
             <p className="text-lg text-textMain font-mono">
               Hi, I'm Adam Mitro. I'm a full-stack engineer passionate about building projects that keep me on the cutting edge of programming languages and technologies.
               I thrive on exploring new tools and frameworks to continuously sharpen my skills and stay ahead in the ever-evolving world of software development.
             </p>
           </div>
         </section>
-        {/* Contact Me Section */}
-        <section className="flex flex-col md:flex-row items-center gap-8 py-6 w-full mx-auto mb-20">
-          <div className="bg-cardBg p-6  border-y border-secondary w-full">
-            <h2 className="text-secondary text-3xl font-bold mb-4 font-mono">Contact Me</h2>
+        {/* Education Section */}
+        <section id="education" className="flex flex-col md:flex-row items-center gap-8 py-6 w-full mx-auto mb-20">
+          <div className="bg-cardBg p-6 border-y border-secondary shadow-lg w-full">
+            <h2 className="text-secondary text-3xl font-bold mb-4 font-mono">Education</h2>
+            <p className="text-lg text-textMain font-mono">
+              University of Pittsburgh, B.S. in Computer Science
+            </p>
+            <p className="text-lg text-textMain font-mono">Start: 2013</p>
+            <p className="text-lg text-textMain font-mono">End: 2017</p>
+            <p className="text-lg text-textMain font-mono">GPA: 3.3; Honors Graduate</p>
+          </div>
+        </section>
+        {/* Contact Info Section */}
+        <section id="contactInfo" className="flex flex-col md:flex-row items-center gap-8 py-6 w-full mx-auto mb-20">
+          <div className="bg-cardBg p-6  border-y border-secondary w-full shadow-lg">
+            <h2 className="text-secondary text-3xl font-bold mb-4 font-mono">Contact Info</h2>
             <p className="text-lg text-textMain font-mono">
               Email - <a href="mailto:adam.mitro@gmail.com" className="text-secondary hover:text-white">adam.mitro@gmail.com</a>
             </p>
@@ -165,8 +204,8 @@ const AboutMe: React.FC = () => {
         </section>
         <div className="flex flex-col md:flex-column items-center gap-8 p-6 max-w-7xl mx-auto mb-20">
           <h2 className="text-secondary text-3xl font-bold mb-4 font-mono">Credit</h2>
-          <p>Icons by Icons8 —
-            <a href="https://icons8.com" className="text-secondary hover:text-white"> https://icons8.com </a></p>
+          <p className="text-textMain">Icons by Icons8 —
+            <a href="https://icons8.com" className="text-secondary hover:text-textMain"> https://icons8.com </a></p>
         </div>
       </div>
     </div>
